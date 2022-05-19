d06ffb73db32f8aeaed16d0df2034b862429c40c
EugeneDar <evgenedar@mail.ru>
2022-05-19 16:20:03 +0300

    concat method changed, unite-all method created

---
+++ src/main/java/EOorg/EOeolang/EOmp/EOconcat.java
@@ -0,0 +1,24 @@
+package EOorg.EOeolang.EOmp;
+
+import org.eolang.*;
+
+import java.util.Arrays;
+
+@SuppressWarnings("PMD.AvoidDollarSigns")
+public class EOconcat extends PhDefault {
+
+    @SuppressWarnings("PMD.ConstructorOnlyInitializesOrCallOtherConstructors")
+    public EOconcat(final Phi sigma) {
+        super(sigma);
+        this.add("arr1", new AtFree());
+        this.add("arr2", new AtFree());
+        this.add("φ", new AtComposite(this, rho -> {
+            final Phi[] first = new Dataized(rho.attr("arr1").get()).take(Phi[].class);
+            final Phi[] second = new Dataized(rho.attr("arr2").get()).take(Phi[].class);
+            final Phi[] result = Arrays.copyOf(first, first.length + second.length);
+            System.arraycopy(second, 0, result, first.length, second.length);
+            return new Data.ToPhi(result);
+        }));
+    }
+
+}
