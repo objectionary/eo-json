44024940869dd5622b5d9bf265ed4b961430f4de
EugeneDar <evgenedar@mail.ru>
2022-07-04 15:37:06 +0300

    the table is rebuilt (didn't check for correctness)

--- src/main/java/EOorg/EOeolang/EOmp/EOrebuild.java
+++ src/main/java/EOorg/EOeolang/EOmp/EOrebuild.java
@@ -1,6 +1,7 @@
 package EOorg.EOeolang.EOmp;
-import EOorg.EOeolang.EOint;
 import org.eolang.*;
+
+import java.util.ArrayList;
 
 @SuppressWarnings("PMD.AvoidDollarSigns")
 public class EOrebuild extends PhDefault {
@@ -14,17 +15,41 @@ public class EOrebuild extends PhDefault {
             final Phi[] harr = new Dataized(rho.attr("harr").get()).take(Phi[].class);
             final Phi[] arr = new Dataized(rho.attr("arr").get()).take(Phi[].class);
 
+            ArrayList<Integer> hashes = new ArrayList<>();
+
             for (final Phi item : harr) {
                 final Long x = new Dataized(item).take(Long.class);
-                System.out.println(x);
-//                final Phi x = new Param(item).strong(Phi.class);
+                hashes.add(Math.toIntExact(x));
             }
 
-//            new PhWith( new EOmap(rho), 0, new Data.ToPhi(dest) )
+            // This value can be changed for memory or speed optimization
+            int TABLE_SIZE = hashes.size();
 
-//            final Phi phi = new Data.ToPhi(arr);
+            ArrayList<ArrayList<Phi>> table = new ArrayList<>();
 
-            return new Data.ToPhi(arr);
+            for (int i = 0;i < TABLE_SIZE;++i) {
+                table.add(new ArrayList<>());
+            }
+
+            for (int i = 0;i < arr.length;++i) {
+                table.get(hashes.get(i) % TABLE_SIZE).add(arr[i]);
+            }
+
+            Phi[] result = new Phi[TABLE_SIZE];
+
+            System.out.println("HERE 3");
+
+            for (int i = 0;i < TABLE_SIZE;++i) {
+                Phi[] array = new Phi[table.get(i).size()];
+                for (int j = 0;j < table.get(i).size();++j) {
+                    array[j] = table.get(i).get(j);
+                }
+                result[i] = new Data.ToPhi(array);
+            }
+
+            System.out.println("HERE 4");
+
+            return new Data.ToPhi(result);
         }));
     }
 
