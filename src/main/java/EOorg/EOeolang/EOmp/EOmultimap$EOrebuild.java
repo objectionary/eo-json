94dcc883906171db4227a2f3c824267a7b8865cc
EugeneDar <evgenedar@mail.ru>
2022-07-06 13:58:32 +0300

    rebuild method moved to multimap

--- src/main/java/EOorg/EOeolang/EOmp/EOmultimap$EOrebuild.java
+++ src/main/java/EOorg/EOeolang/EOmp/EOmultimap$EOrebuild.java
@@ -23,15 +23,16 @@
  */
 
 package EOorg.EOeolang.EOmp;
+
 import org.eolang.*;
 
 import java.util.ArrayList;
 
 @SuppressWarnings("PMD.AvoidDollarSigns")
-public class EOrebuild extends PhDefault {
+public class EOmultimap$EOrebuild extends PhDefault {
 
     @SuppressWarnings("PMD.ConstructorOnlyInitializesOrCallOtherConstructors")
-    public EOrebuild(final Phi sigma) {
+    public EOmultimap$EOrebuild(final Phi sigma) {
         super(sigma);
         this.add("harr", new AtFree());
         this.add("arr", new AtFree());
@@ -51,19 +52,19 @@ public class EOrebuild extends PhDefault {
 
             ArrayList<ArrayList<Phi>> table = new ArrayList<>();
 
-            for (int i = 0;i < TABLE_SIZE;++i) {
+            for (int i = 0; i < TABLE_SIZE; ++i) {
                 table.add(new ArrayList<>());
             }
 
-            for (int i = 0;i < arr.length;++i) {
+            for (int i = 0; i < arr.length; ++i) {
                 table.get(hashes.get(i) % TABLE_SIZE).add(arr[i]);
             }
 
             Phi[] result = new Phi[TABLE_SIZE];
 
-            for (int i = 0;i < TABLE_SIZE;++i) {
+            for (int i = 0; i < TABLE_SIZE; ++i) {
                 Phi[] array = new Phi[table.get(i).size()];
-                for (int j = 0;j < table.get(i).size();++j) {
+                for (int j = 0; j < table.get(i).size(); ++j) {
                     array[j] = table.get(i).get(j);
                 }
                 result[i] = new Data.ToPhi(array);
